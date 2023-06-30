ffe70ce0f4c6a041e180518b4c15c512b28711a2
josediaz449 <josediaz449@yahoo.com>
2023-06-30 11:58:22 -0400

    Sorted completed items by date DESC. Added copy button to Completed List.

--- src/main/java/com/example/application/views/todo/ToDoList.java
+++ src/main/java/com/example/application/views/todo/ToDoList.java
@@ -49,6 +49,7 @@ public class ToDoList extends ToDoGrid {
     @Override
     void updateList() {
         List<ToDoItem> toDoItems = toDoItemService.getAllNotCompletedToDoItems();
+        grid.setItems(toDoItems);
         grid.setAllRowsVisible(true);
     }
 }
